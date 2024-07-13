81edd1e35a0565d929c003a03e069ee474d88666
Damien Davis-Neff <damiendavisneff@gmail.com>
2024-07-12 22:45:52 -0400

    Delete install.bat

--- install.bat
+++
@@ -1,9 +0,0 @@
-::# make sure to add all npm modules in here
-::# or include ./node_modules
-:: RUN THIS COMMAND FIRST: npm init -y
-@echo off
-echo Beginning Install Process
-npm i discord.js
-npm i dotenv
-echo Completed Install Process
-pause
